2d8abfc2da7d9abc1c1084fa1cd1669e8209b153
theadityasc30 <aditya05prasad@gmail.com>
2020-11-01 11:26:50 -0500

    added a sample op mode called AdityaSimpleOpMode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdityaSimpleOpMode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AdityaSimpleOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        this.waitForStart();
+
+        if (this.opModeIsActive()){
+            while (opModeIsActive()) {
+                telemetry.addData("runtime", )
+            }
+        }
+    }
+}
